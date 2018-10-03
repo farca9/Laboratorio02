26e4f5b2244860fdf95b29a35344fa43db2f1b2a
Facundo Arca <farca9@gmail.com>
2018-10-03 17:16:34 -0300

    Lab 03 - Parte 2 - Punto 2
    Realizado hasta el punto 2 inclusive. Cambiado el icono de la aplicacion.

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/EstadoPedidoReceiver.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/EstadoPedidoReceiver.java
@@ -1,9 +1,16 @@
 package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
 
 import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.PedidoRepository;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Pedido;
@@ -30,7 +37,36 @@ public class EstadoPedidoReceiver extends BroadcastReceiver {
                 if(intent.hasExtra("idPedido")){
 
                     Pedido pedido = pedidoRepository.buscarPorId(intent.getIntExtra("idPedido",-1));
-                    Toast.makeText(context, "Pedido para "+pedido.getMailContacto()+" ha cambiado de estado a ACEPTADO", Toast.LENGTH_SHORT).show();
+
+                    //LAB 03 - pt1
+                    //Toast.makeText(context, "Pedido para "+pedido.getMailContacto()+" ha cambiado de estado a ACEPTADO", Toast.LENGTH_SHORT).show();
+
+                    //LAB 03 - pt2
+                        Intent i = new Intent(context,NuevoPedidoActivity.class);
+                        i.putExtra("idSeleccionado",pedido.getId());
+                        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"CANAL01");
+
+                        builder.setContentTitle("Tu pedido fue aceptado");
+
+                        String descripcion = "El costo sera de $"+pedido.total().toString();
+                        if(!pedido.getRetirar()){
+                            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                            descripcion+="\nHora de envio prevista: "+sdf.format(pedido.getFecha());
+                        }else{
+                            descripcion+="\nRetirar en el local";
+                        }
+
+                        builder.setStyle( new NotificationCompat.BigTextStyle().bigText(descripcion));
+
+                        builder.setSmallIcon(R.drawable.shrimp);
+
+                        builder.setContentIntent(PendingIntent.getActivity(context,0,i, PendingIntent.FLAG_UPDATE_CURRENT));
+
+                        builder.setAutoCancel(true);
+
+                        NotificationManagerCompat.from(context).notify(1,builder.build());
 
                 }
 
